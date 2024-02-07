85fbbc80adb2342dd01ee26a10638a2bc62a45a8
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-02-07 00:22:35 -0300

    update

--- desafios/aluraplay/js/createVideo.js
+++ desafios/aluraplay/js/createVideo.js
@@ -8,9 +8,13 @@ async function creatVideo(event) {
   const url = document.querySelector("[data-url]").value;
   const title = document.querySelector("[data-title]").value;
   const description = Math.floor(Math.random() * 10).toString();
-
+ 
   await connectsApi.creatVideos(title, description, url, imagem);
-  
+  console.log(imagem);
+  console.log(url);
+  console.log(title);
+  console.log(description);
+  console.log(creatVideo(event));
 
   window.location.href = "../pages/envio-concluido.html";
 }
